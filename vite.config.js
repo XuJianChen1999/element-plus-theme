594feed7f65be63494c5be4e00023d5a97a12547
徐建辰 <18253997748@163.com>
2022-06-27 11:11:26 +0800

    配置vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,68 @@
-import { fileURLToPath, URL } from 'url'
+/*
+ * @Description: vite配置
+ * @Author: xjc
+ * @Date: 2022-06-27 10:53:52
+ * @LastEditTime: 2022-06-27 11:10:52
+ * @LastEditors: xjc
+ */
+import path from 'path'
+import {fileURLToPath, URL} from 'url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import Unocss from 'unocss/vite'
+import {
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+// eslint-disable-next-line no-undef
+const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ]
+    }),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          scale: 1.2,
+          warn: true,
+        }),
+      ],
+      transformers: [
+        transformerDirectives(),
+        transformerVariantGroup(),
+      ]
+    })
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~/': `${pathSrc}/`,
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/element/index.scss" as *;`,
+      }
     }
   }
 })
